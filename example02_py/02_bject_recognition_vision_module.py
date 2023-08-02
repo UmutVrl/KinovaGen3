9e8bea5c71a7897df275d0effe3704087b130c52
UmutVrl <umutcan.vural@gmail.com>
2023-08-02 11:52:02 +0200

    second example, vision module

---
+++ example02_py/02_bject_recognition_vision_module.py
@@ -0,0 +1,14 @@
+##########################################################
+#    Camera Module Example for Kinova Gen3 Robotic Arm   #
+#    written by: U. Vural                                #
+#    based on: GitHub:Kinovarobotics/kortex              #
+#                                                        #
+#    for KISS Project at Furtwangen University           #
+#                                                        #
+##########################################################
+#    specs:                                              #
+#    Python 3.9                                          #
+#    Kinova Kortex 2.6.0                                 #
+#    Gen3 firmware Bundle 2.5.2-r.2                      #
+#                                                        #
+##########################################################
