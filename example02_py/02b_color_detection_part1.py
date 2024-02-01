c8c207d3055728c4f6829bbbef87fd310d244296
Umut Vural <65653101+UmutVrl@users.noreply.github.com>
2024-02-01 11:56:41 +0100

    example_02 vision module examples

---
+++ example02_py/02b_color_detection_part1.py
@@ -0,0 +1,19 @@
+##########################################################
+#    Kinova Gen3 Robotic Arm                             #
+#    Color detection Sample Part 1                       #
+#    Calibration of Camera HVR for green color           #
+#    See 02b_color_detection_part2 for color detection   #
+#                                                        #
+#    written by: U. Vural                                #
+#    based on: GitHub:Kinovarobotics/kortex              #
+#                                                        #
+#    for KISS Project at Furtwangen University           #
+#                                                        #
+##########################################################
+#    specs:                                              #
+#    Python 3.9                                          #
+#    Kinova Kortex 2.6.0                                 #
+#    Gen3 firmware Bundle 2.5.2-r.2                      #
+#                                                        #
+#                                                        #
+##########################################################
---
+++ example02_py/02b_color_detection_part2.py
@@ -0,0 +1,20 @@
+##########################################################
+#    Kinova Gen3 Robotic Arm                             #
+#    Color detection Sample Part 2                       #
+#                                                        #
+#    Green color detection with the Robot Cam            #
+#    See 02b_color_detection_part1 for color calibration #
+#                                                        #
+#    written by: U. Vural                                #
+#    based on: GitHub:Kinovarobotics/kortex              #
+#                                                        #
+#    for KISS Project at Furtwangen University           #
+#                                                        #
+##########################################################
+#    specs:                                              #
+#    Python 3.9                                          #
+#    Kinova Kortex 2.6.0                                 #
+#    Gen3 firmware Bundle 2.5.2-r.2                      #
+#                                                        #
+#                                                        #
+##########################################################
