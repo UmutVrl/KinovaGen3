667bb49e2dbc06997344502af35a08e5fd2315e7
Umut Vural <65653101+UmutVrl@users.noreply.github.com>
2024-04-02 12:22:31 +0200

    example_02 vision module YOLOv8

--- example02_py/02b_color_detection_part1.py
+++ example02_py/02b_color_detection_part1.py
@@ -30,19 +30,26 @@ write_path = os.getcwd() + "/resources/calibration_screenshot"
 while True:
     ret, frame = source.read()
     frame = cv2.resize(frame, (640, 480), 3)
+
+    if not ret or cv2.waitKey(1) == ord("q"):  # press q to exit
+        break
+
     if ret:
-        #print("Frame Shape", frame.shape)
+        # print("Frame Shape", frame.shape)
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         cv2.imshow("Output video", frame)
         cv2.imshow("grayscale video", frame_gray)
-        if cv2.waitKey(1) & 0xFF == ord("1"):
+        if cv2.waitKey(1) & 0xFF == ord("s"):  # press s to take screenshot
             cv2.imwrite(write_path + str(count) + ".jpg", frame)
             cv2.rectangle(frame, (0, 200), (640, 300), (0, 255, 0), cv2.FILLED)
             cv2.putText(frame, "Scan Saved", (150, 265), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
             cv2.imshow("Output video", frame)
             print("Frame Shape", frame.shape)
-            cv2.waitKey(100)
-            count = +1
+            cv2.waitKey(30)
+            count += 1
     else:
         break
+
+source.release()
+cv2.destroyAllWindows()
