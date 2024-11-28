684bcf64ce498b279ed48ea662de50df86228b94
Umut Vural <65653101+UmutVrl@users.noreply.github.com>
2024-11-28 15:07:31 +0100

    bug_fix screenshot_taker

--- example03_py/Screenshot_Taker_timer.py
+++ example03_py/Screenshot_Taker_timer.py
@@ -3,7 +3,6 @@
 #                                                        #
 #    Taking timed screenshots from the camera            #
 #                                                        #
-#    see chessboard pdf under resources folder           #
 #                                                        #
 #    written by: U. Vural                                #
 #                                                        #
@@ -21,37 +20,60 @@ import cv2
 import os
 import time
 
-source = cv2.VideoCapture("rtsp://192.168.1.10/color")
-source.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-count = 0
-write_path = os.getcwd() + "/resources/calibration_screenshots"
+# Import the utilities helper module of Kinova
+import argparse
+import utilities
 
-# Initialize the time tracker
-last_capture_time = time.time()
 
-while cv2.waitKey(1) != 27:  # press ESC to exit
-    ret, frame = source.read()
-    frame = cv2.resize(frame, (1280, 720), 3)
-    # Note: Be careful with frame dimensions. These have to match with the main pick&place program.
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    args = utilities.parseConnectionArguments(parser)
 
-    if ret:
-        # print("Frame Shape", frame.shape)
-        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Create connection to the device and get the router
+    with utilities.DeviceConnection.createTcpConnection(args) as router:
 
-        cv2.imshow("Output Video", frame)
-        # cv2.imshow("Grayscale Video", frame_gray)
-        if time.time() - last_capture_time > 3:  # More than 5 seconds have passed
-            cv2.imwrite(write_path + str(count) + ".jpg", frame)
-            cv2.rectangle(frame, (0, 200), (1280, 300), (0, 255, 0), cv2.FILLED)
-            cv2.putText(frame, "Scan Saved", (150, 265), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
-            cv2.imshow("Output Video", frame)
-            print("Scan Saved. Frame Shape:", frame.shape)
-            cv2.waitKey(30)  # Waiting duration between each screenshot.
-            # Take many screenshots with different position & angle combinations for better precision (100+)
-            count += 1
-            last_capture_time = time.time()  # Reset the last capture time
-    else:
-        break
+        # Camera Streaming via Ethernet
+        source = cv2.VideoCapture("rtsp://192.168.1.10/color")
+        source.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 
-    source.release()
-    cv2.destroyAllWindows()
+        count = 0
+        frame_width = 1280
+        frame_height = 720
+        write_path = os.getcwd() + "/resources/calibration_screenshots_snickers_"
+
+        # Initialize the time tracker
+        last_capture_time = time.time()
+
+        while cv2.waitKey(1) != 27:  # press ESC to exit
+            has_frame, frame = source.read()
+            frame = cv2.resize(frame, (frame_width, frame_height), 3)
+            # Note: Be careful with frame dimensions. These have to match with the main pick&place program.
+
+            if has_frame:
+                # print("Frame Shape", frame.shape)
+                frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                cv2.imshow("Output Video", frame)
+                # cv2.imshow("Grayscale Video", frame_gray)
+                if time.time() - last_capture_time > 3:  # More than 5 seconds have passed
+                    cv2.imwrite(write_path + str(count) + ".jpg", frame)
+                    cv2.rectangle(frame, (0, frame_height//3), (frame_width, frame_height//2), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(frame, "Scan Saved", (frame_width//2-120, frame_height//2-40),
+                                cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 3)
+                    cv2.imshow("Output Video", frame)
+                    print("Scan Saved. Frame Shape:", frame.shape)
+                    cv2.waitKey(40)  # Waiting duration between each screenshot.
+                    # Take many screenshots with different position & angle combinations for better precision (100+)
+                    count += 1
+                    last_capture_time = time.time()  # Reset the last capture time
+            else:
+                print("No frame")
+                break
+
+        source.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
